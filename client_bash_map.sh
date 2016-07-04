92c43ed28e84a7599b2586de9e058a5fc8a82c93
Tim_Bleidorn <tim.bleidorn@outlook.com>
2016-07-03 18:12:48 -0600

    test

--- client_bash_map.sh
+++ client_bash_map.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Introduction
-echo "This is a Single source / destination threading application 
-with a ratio of 100:1 source/destination ports per thread. 
+echo "This is a Single source / destination threading application
+with a ratio of 100:1 source/destination ports per thread.
 Ensure you have the server running on the destination host"
 
 # Read the destination IP address.
@@ -10,7 +10,7 @@ echo "What is the IP of the destination?"
 read dst
 
 # Number of threads
-echo "This script will initiate 100 streams per thread, how many threads 
+echo "This script will initiate 100 streams per thread, how many threads
 should be initiated?"
 read sockets
 
@@ -25,7 +25,6 @@ while [ $counter -le $sockets ]; do
 	iperf3 -c $dst -p $dst_port -P 100 -i 10 -t $t_time &
 	let counter=counter+1
 	let dst_port=dst_port+1
-done 
+done
 
-
-
+#test
